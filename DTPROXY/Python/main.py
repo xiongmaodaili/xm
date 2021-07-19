40427550573a17f613fceb1450d2789bda8a04ca
xm <system@mengqikeji.cn>
2021-07-19 15:53:13 +0800

    20210719：python3代码更改

--- DTPROXY/Python/main.py
+++ DTPROXY/Python/main.py
@@ -38,7 +38,7 @@ print(auth)
 #http协议的网站用此配置
 proxy = {"http":"http://" + ip_port}
 #https协议的网站用此配置
-#proxy = {"https": "https://" + ip_port}
+#proxy = {"https": "http://" + ip_port}
 print(proxy)
 headers = {"Proxy-Authorization": auth,"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"}
 #r = requests.get("http://2021.ip138.com", headers=headers, proxies=proxy, verify=False,allow_redirects=False)
